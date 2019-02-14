f05b7496fb2960c6b3dd2ab8bec0df09dd5dbe2b
Stefan Keler <stefankeler96@gmail.com>
2019-02-14 23:59:25 +0100

    test

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try{
+                StreamWriter sr = new StreamWriter("");
+                sr.Write("");
+                // some exception occurs here 
+                File.SetAttributes("", FileAttributes.Hidden);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
